7c67a83ad0bb6a69f39e628d50e0b8843028a4bb
Sammy Kroner <sammyk118@gmail.com>
2020-12-18 18:59:21 -0800

    now with functional arrays (switched from concat to push)

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  getPasswordChars();
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -19,24 +20,63 @@ var lowerArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm
 var upperArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var numArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 var symArray = ['!', '\"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~',];
-
+var passwordLength;
 //newArray = lowerArray.concat(numArray);
 
 
 function generatePassword() {
-  if (confirm("lowercase?")) 
-    masterArray.concat(lowerArray);
-  
-  if (confirm("uppercase?"))
-    masterArray.concat(upperArray);
+  // getPasswordChars();
 
-  if (confirm("numeric?"))
-    masterArray.concat(numArray);
-  
-  if (confirm("symbols?"))
-    masterArray.concat(symArray);
 }
 
+
+
+function getPasswordChars() {
+  if (confirm("lowercase?"))
+    Array.prototype.push.apply(masterArray, lowerArray);
+    console.log(masterArray);
+
+  if (confirm("uppercase?"))
+    Array.prototype.push.apply(masterArray, upperArray);
+    console.log(masterArray);
+  
+  if (confirm("numeric?"))
+    Array.prototype.push.apply(masterArray, numArray);
+    console.log(masterArray);
+
+  if (confirm("symbols?"))
+    Array.prototype.push.apply(masterArray, symArray);
+    console.log(masterArray)
+  if (masterArray.length == 0)
+    alert("You must say yes to at least one prompt!");
+  else
+    getPasswordLength();
+}
+
+function getPasswordLength() {
+  passwordLength = prompt("Desired password length?");
+  if (typeof passwordLength ==! 'number')
+    alert("Please enter a number");
+
+  else if (passwordLength < 8 || passwordLength > 128)
+    alert("Please enter a number between 8 and 128");
+
+  else
+    generate();
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function generate() {
+  for (var i = 0; i < passwordLength - 1; i++) {
+    getRandomInt(masterArray.length - 1)
+
+  }
+
+
+}
 
 // create array for each character type
 // confirm each prompt, if true, push every lowercase/uppercase/numeric/symbol array into master array. store password length
